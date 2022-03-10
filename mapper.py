003dd9597bcc0f676b39361df7d0e24d6542e469
maxatnurgazin <nurgazin.maksat@gmail.com>
2022-03-10 14:30:44 +0600

    Just testing github online vscode

--- mapper.py
+++ mapper.py
@@ -3,10 +3,11 @@
 import sys
 from re import sub
 
+#read lines from stdin one by one, and for each line
 for line in sys.stdin:
     #use regex to remove unnecessary characters: [^\w\s] chooses chars that are not alphanumeric and space.
     line = sub('[^\w\s]', '', line).strip()
 
-    #split the line and print it.
+    #split the line and print it as a (key value) pair.
     for word in line.split():
         print(f'{word} 1')
